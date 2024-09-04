1f78c5098ecca8540fac5a7b4dd514c9f40dca21
Ben Keller <malzraa@gmail.com>
2024-09-04 14:33:20 -0500

    Started a very bare-bones feedback yields class.

---
+++ arsenal_gear/feedbacks/__init__.py
@@ -0,0 +1,35 @@
+"""
+feedbacks
+=========
+
+Functions for the returns from various feedback mechanisms (SN, winds, radiation, etc.)
+"""
+
+
+from typing import Type
+
+import astropy.units as u
+import numpy as np
+from astropy.units import Quantity
+
+
+class FBMechanism():
+    def __init__(self, start:Quantity["time"], end:Quantity["time"]) -> None:
+        self.start = start
+        self.end = end
+        pass
+
+    def radiation(self, bands:np.float64): -> np.float64 # What units should we put out here for each band? photons? energy?
+        pass
+
+    def ejecta_mass(self) -> Quantitity["mass"]:
+        pass
+
+    def ejecta_velocity(self) -> Quantitity["velocity"]:
+        pass
+
+    def metal_yields(self) -> Quantity["mass"]:
+        pass
+
+    def species_yields(self, species:list[str]) -> Quantity["mass"]:
+        pass
