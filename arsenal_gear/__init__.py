f463602aa1ddf4c9c0d745aac898f72158a0663e
Lachlan Lancaster <lachlanlancaster@gmail.com>
2024-12-03 21:40:10 -0500

    initial function to create N_SN distribution

--- arsenal_gear/__init__.py
+++ arsenal_gear/__init__.py
@@ -8,6 +8,7 @@ relevant for stellar feedback from massive stars.
 
 from . import dist_funcs, feedbacks, population, stellar_evolution
 
+import numpy as np
 import astropy.units as u
 from astropy.units import Quantity
 
@@ -22,19 +23,37 @@ class StellarPopulation():
     pre-calculated parameters for the feedback and radiaiton.
     """
 
-    def __init__(self) -> None:
+    def __init__(self, discrete:bool=True) -> None:
         self.Mtot = 1e6*u.Msun
-        self.imf = dist_funcs.imf.Salpeter(0.08*u.Msun, 100*u.Msun)
+        self.imf = dist_funcs.imf.Salpeter(0.08*u.Msun, 100*u.Msun, alpha=2.3)
+        self.discrete = discrete
         # relative to solar
         self.metallicity = 1.0
         # generate masses
-        self.masses = self.imf.sample(self.Mtot)
+        if self.discrete:
+            self.masses = self.imf.sample(self.Mtot)
         self.tmin = 0.0*u.Myr
         self.tmax = 40.0*u.Myr
 
         mbase = "/Users/lachlanlancaster/Documents/Columbia/Research/pop_synth/wind_pop_synthesis/mist_iso_theory/MIST_v1.2_vvcrit0.0_full_isos/"
         isofname = mbase + "MIST_v1.2_feh_p0.00_afe_p0.0_vvcrit0.0_full.iso"
         self.iso = stellar_evolution.isochrone.MIST(isofname)
+
+    def get_NSN(self, t:Quantity["time"]) -> int:
+        """
+        Return the number of supernovae that have gone off by time t
+        """
+        Mmax = self.iso.get_Mmax(t)
+        if self.discrete:
+            nsn = np.array([len(np.intersect1d(np.where(self.masses > m),np.where(self.masses > 8*u.Msun))) for m in Mmax])
+        else:
+            # fraction of stars that have exploded
+            fexp_8 = 1 - self.imf.cdf(8*u.Msun)
+            fexp = 1 - self.imf.cdf(Mmax)
+            fexp = fexp*(fexp < fexp_8) + fexp_8*(fexp >= fexp_8)
+            nsn = (fexp*self.Mtot/self.imf.mavg).to("").value
+        return nsn
+    
 
     def __call__(self, N:int) -> population.StarPopulation:
         """
