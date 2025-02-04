f9e38677544960029171586bd8d9255e0a372728
Claude Cournoyer-Cloutier <cournoyc@mcmaster.ca>
2025-02-04 16:33:55 -0500

    Add tests for a, q, e based on IMF tests

--- tests/test_binaries.py
+++ tests/test_binaries.py
@@ -1,39 +1,64 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Jan 13 14:46:25 2025
-
-@author: claude
-"""
-
-from typing import Type
-
 import astropy.units as u
 import numpy as np
-from astropy.units import Quantity
-from scipy.stats import rv_continuous, rv_histogram
+import pytest
+from scipy.stats import sampling
+from numpy.testing import assert_allclose
 
 import arsenal_gear
 
-def test_fraction():
-    # Create a StarPopulation
-    mass = u.Msun*np.ones(100) 
-    mass[50:] *= 20 # Change half the stars into massive stars
-    metals = 0.1*np.ones(100)
-    tform = u.Myr*np.zeros(100)
-    stars = arsenal_gear.population.StarPopulation(mass=mass,metals=metals,tform=tform)
-    #print(star)
-    # Test the binary fraction sampler
-    _binaries = arsenal_gear.dist_funcs.binaries.StepFraction([0, 1], 10*u.Msun, stars).sample()
-    #print(_binaries)
-    # Create the BinaryPopulation
-    primaries = arsenal_gear.population.StarPopulation(mass=mass[_binaries],metals=metals[_binaries],tform=tform[_binaries])
-    print(primaries)
-    companions = arsenal_gear.population.StarPopulation(mass=0.5*mass[_binaries],metals=metals[_binaries],tform=tform[_binaries])
-    semimajor = u.au*np.ones(len(primaries))
-    eccentricity = np.zeros(len(primaries))
-    binaries = arsenal_gear.population.BinaryPopulation(primary=primaries, secondary=companions, 
-                                                        semimajor=semimajor, eccentricity=eccentricity)
-    print(binaries)
+class TestLogUniformSemimajor:
+    N = int(1e6)
+    min_a = 0.1*u.au
+    max_a = 100*u.au
+    sma = arsenal_gear.dist_funcs.binaries.LogUniformSemimajor(min_a, max_a)
+    
+    def test_limits(self):
+        a = self.sma.sample(self.N)
+        assert a.max() <= self.max_a
+        assert a.min() >= self.min_a
 
-test_fraction()
+    def test_count(self):
+        a = self.sma.sample(self.N)
+        assert len(a) == self.N
+
+    def test_mean(self):
+        a = self.sma.sample(self.N)
+        assert_allclose(np.mean(a), u.au*self.sma.mean(), rtol=0, atol=5*self.sma.var())
+
+class TestUniformMassRatio:
+    N = int(1e6)
+    min_q = 0.1
+    max_q = 1
+    mratio = arsenal_gear.dist_funcs.binaries.UniformMassRatio(min_q, max_q)
+    
+    def test_limits(self):
+        q = self.mratio.sample(self.N)
+        assert q.max() <= self.max_q
+        assert q.min() >= self.min_q
+
+    def test_count(self):
+        q = self.mratio.sample(self.N)
+        assert len(q) == self.N
+
+    def test_mean(self):
+        q = self.mratio.sample(self.N)
+        assert_allclose(np.mean(q), self.mratio.mean(), rtol=0, atol=5*self.mratio.var())
+
+class TestUniformEccentricity:
+    N = int(1e6)
+    min_e = 0
+    max_e = 0.9
+    ecc = arsenal_gear.dist_funcs.binaries.UniformEccentricity(min_e, max_e)
+    
+    def test_limits(self):
+        e = self.ecc.sample(self.N)
+        assert e.max() <= self.max_e
+        assert e.min() >= self.min_e
+
+    def test_count(self):
+        e = self.ecc.sample(self.N)
+        assert len(e) == self.N
+
+    def test_mean(self):
+        e = self.ecc.sample(self.N)
+        assert_allclose(np.mean(e), self.ecc.mean(), rtol=0, atol=5*self.ecc.var())
