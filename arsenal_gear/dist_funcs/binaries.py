668a7ca44b16ed672c9da92a28c686383d48f22a
Claude Cournoyer-Cloutier <cournoyc@mcmaster.ca>
2025-06-24 15:16:15 -0400

    Initial test for sampling from BPASS probs

--- arsenal_gear/dist_funcs/binaries.py
+++ arsenal_gear/dist_funcs/binaries.py
@@ -11,7 +11,157 @@ import numpy as np
 from astropy.units import Quantity
 from scipy.stats import rv_continuous, loguniform, uniform
 
-from arsenal_gear.population import StarPopulation
+from arsenal_gear.population import StarPopulation, BinaryPopulation
+
+class BinaryDistributions:
+    """
+    This class is the superclass of all distributions of binary properties
+    in which the binary fraction and distributions of orbtial periods and 
+    mass ratios are not independent. This samples from a 3D probability
+    distribution that incldues both binaries and single stars. 
+    This can also include a user-specified mass limit above which all 
+    binaries have orbital periods log(P/days) <= 3.8, to account for the 
+    inner companion in hierarchical systems.
+
+    :param model: BPASS model, see BPASS manual for details
+    :type model: str
+    :param m_inner: Mass above which log(P/days) <= 3.8
+    :type m_inner: astropy mass unit
+    :param model: BPASS directory (likely on scratch, given the size)
+    :type model: str
+
+    """
+    def __init__(self, model: str, m_inner: Quantity["mass"], bpass_dir: str):
+        self.models = ['100_100', '100_300', 
+                       '135_100', '135_300', '135all_100',
+                       '170_100', '170_300', 
+                       'chab100', 'chab300']
+        if model not in self.models:
+            raise ValueError(f"Model {model} does not exist.")
+        if m_inner < (9 * u.Msun):
+            raise Warning(f"There are inner companions with log(P/days) \ 
+            > 3.8 for masses below 9 Msun. You are overestimating the \
+            number of close companions for low- and intermediate-mass \
+            stars compared to observations.")
+        self.model:    str   = model
+        self.m_inner:  float = min_mass.to(u.Msun).value
+        self.dir:      str   = bpass_dir + '/'
+        
+        
+    def generate_probabilities(self):
+        """
+        Generate the appropriate 3D grid of probabilities from the
+        BPASS input file.
+
+        """
+        m1_values = np.concatenate((np.arange(0.1, 0.7, 0.1), 
+                                    np.arange(0.8, 2.2, 0.1),
+                                    np.array([2.3, 2.5, 2.7, 3, 3.2, 3.5, 3.7]), 
+                                    np.arange(4, 10, 0.5), 
+                                    np.arange(10, 26, 1), 
+                                    np.array([30, 35, 40, 50, 60, 70, 80, 100, 
+                                              120, 150, 200, 300])))
+        
+        q_values    = np.arange(0, 1, 0.1) # q = 0 for single
+        logp_values = np.arange(0, 4.4, 0.2) # 4.2 w/ q = 0 for single
+        
+        file_path = self.dir + 'bpass_v2.2.1_imf' + self.model + '/input_bpass_z014_bin_' + self.model
+        
+        with open(file_path, 'r') as f:
+            prob = np.zeros((len(m1_values), len(q_values), len(logp_values)))
+            save_next = False
+            save_which = None
+            for line in f:
+                ln = line.strip()
+                if 'NEWSINMODS' in ln:
+                    # e.g. NEWSINMODS/z014/sneplot-z014-300
+                    # Read only mass
+                    save_next = True
+                    mass = float(ln[ln.rfind('-') + 1:])
+                    save_which = np.array([np.argmin(np.abs(m1_values - mass)), 0, -1])
+                elif 'NEWBINMODS/NEWBINMODS' in ln:
+                    # e.g. NEWBINMODS/NEWBINMODS/z014/sneplot-z014-300-0.1-0
+                    # Read period, mass ratio, and mass
+                    save_next = True
+                    ind_p = ln.rfind('-')
+                    ind_q = ln[:ind_p].rfind('-')
+                    ind_m = ln[:ind_q].rfind('-')
+                    logp = float(ln[ind_p + 1:])
+                    qrat = float(ln[ind_q + 1:ind_p])
+                    mass = float(ln[ind_m + 1:ind_q])
+                    save_which = np.array([np.argmin(np.abs(m1_values - mass)), 
+                                           np.argmin(np.abs(q_values - qrat)), 
+                                           np.argmin(np.abs(logp_values - logp))])
+                elif save_next:
+                    save_next = False
+                    ln = np.array(ln.split())
+                    if prob[save_which[0], save_which[1], save_which[2]] > 0:
+                        prob[save_which[0], save_which[1], save_which[2]] += float(ln[0])
+                        #print('Overwritting value at', m1_values[save_which[0]], 'MSun, q=', q_values[save_which[1]], ', log(P/days)', logp_values[save_which[2]])
+                    else:
+                        prob[save_which[0], save_which[1], save_which[2]] = ln[0]
+            
+            
+        if self.m_inner < int(model[-3:]):
+            # If mass limit for inner companion
+            # Zero out probability above 3.8 if M > m_inner
+            _mask_m = np.where(m1_values >= self.m_inner)[0]
+            _mask_q = np.where((q_values > 0))[0]
+            _mask_p = np.where((logp_values > 3.8))[0]
+            _mask_i = np.where((logp_values <= 3.8))[0]
+            _norm_inner = (prob[_mask_m[0]:, _mask_q[0]:, :_mask_i[-1]]).sum()
+            _norm_outer = (prob[_mask_m[0]:, _mask_q[0]:, _mask_p[0]:]).sum()
+            prob[_mask_m[0]:, _mask_q[0]:, _mask_p[0]:] = np.zeros((len(_mask_m), len(_mask_q), len(_mask_p)))
+            prob[_mask_m[0]:, _mask_q[0]:, :_mask_i[-1]] *= (_norm_inner + _norm_outer)/_norm_inner
+            
+            np.save(self.dir + 'prob_bin_imf' + self.model + '.npy', prob)
+        
+        else:
+            np.save(self.dir + 'prob_bin_imf' + self.model + '.npy', prob)
+                
+            
+    def discrete_sampling(self, mtot: Quantity["mass"]) -> Tuple[Quantity["mass"], 
+                                                                 Quantity["mass"], 
+                                                                 Quantity["mass"], 
+                                                                 Quantity["time"]]:
+        """
+        Draw individual binaries and single stars from a BPASS input file
+        with a given IMF slope and upper mass cut-off, with a target total mass
+
+        :param mtot: Target total mass of the sample
+        :type mtot:  Quantity["mass"]
+        :return:     Single stars' masses 
+        :rtype:      Quantity["mass"]
+        :return:     Primary masses
+        :rtype:      Quantity["mass"]
+        :return:     Orbital periods
+        :rtype:      Quantity["time"]
+        
+        """
+        prob = np.load(self.dir + 'prob_bin_imf' + self.model + '.npy')
+        
+        # Sample by flattening the array
+        # Assume that the average system mass is below 1.5 MSun
+        num_samples = int(1.5 * mtot.to(u.Msun).value)
+        prob_flat = prob.ravel()
+        samp_inds = np.random.choice(a=np.prod(prob.shape), size=num_samples, p=prob_flat)
+        # Convert flattened indices back to 3D coordinates
+        ind_m, ind_q, ind_p = np.unravel_index(samp_inds, prob.shape)
+        samp_m = m1_values[ind_m]
+        samp_q = q_values[ind_q]
+        samp_p = logp_values[ind_p]
+        
+        _target_mass = np.argmin(np.abs(mtot.to(u.Msun).value - (samp_m * (1 + samp_q)).cumsum()))
+        primary_masses   = samp_m[:_target_mass] * u.Msun
+        companion_masses = samp_q[:_target_mass] * samp_m[:_target_mass] * u.Msun
+        orbital_periods  = 10**samp_p[:_target_mass] * u.d
+        
+        _mask_s = np.where(companion_masses == 0)[0]
+        _mask_b = np.where(companion_masses > 0)[0]
+        
+        return primary_masses[_mask_s], primary_masses[_mask_b], companion_masses[_mask_b], orbital_periods[_mask_b] 
+        
+        
 
 class Fraction:
     """
@@ -129,6 +279,60 @@ class UniformMassRatio(MassRatio):
         return rv.ppf(x)
     
 
+class Period(rv_continuous):
+    """
+    This class is the superclass of all orbital period distributions
+    :param min_p: Minimum orbital period
+    :type min_p: astropy time unit
+    :param max_p: Maximum orbital period
+    :type max_p: astropy time unit
+    :param stars: Primaries
+    :type stars: StarPopulation
+    :param name: Name for the scipy.stats rv_continuous instance
+    :type name: str
+    """
+
+    def __init__(self, min_p: Quantity["time"], 
+                 max_p: Quantity["time"], name=""):
+        self.min_p: float = min_p.to(u.d).value
+        self.max_p: float = max_p.to(u.d).value
+        assert self.min_p > 0
+        assert self.max_p > self.min_p
+        super().__init__(a=self.min_p, b=self.max_p, name=name)
+        
+    def sample(self, N: int) -> Quantity["time"]:
+        """
+        :param N: Number of stars to draw
+        :type N: int
+        :return: List of orbital periods
+        :rtype: Quantity["time"]
+        """
+        return self.rvs(size=N)*u.d
+
+
+class LogUniformPeriod(Period):
+    """
+    A simple step function distribution of semi-major axes, with a log-uniform
+    distribution above and below the changeover semi-major axis
+    :param sma: Changeover semi-major axis
+    :type sma: astropy length unit
+    :param ratio: Ratio of probabilities for close and wide binaries
+    :type ratio: float
+    A simple loguniform distribution of semi-major axes
+    """
+    
+    def __init__(self, min_p: Quantity["time"], max_p: Quantity["time"]):
+        self.name = "loguniform"
+        super().__init__(min_p, max_p, name=self.name)
+        
+    def _pdf(self, x: np.float64) -> np.float64:
+        rv = loguniform(self.min_p, self.max_p)
+        return rv.pdf(x)
+   
+    def _ppf(self, x: np.float64) -> np.float64:
+        rv = loguniform(self.min_p, self.max_p)
+        return rv.ppf(x)
+    
     
 class Semimajor(rv_continuous):
     """
