19c6d7e39de8464cbf2ca7e2dcf2b88a03ba63aa
Ben Keller <malzraa@gmail.com>
2026-02-02 10:57:18 -0600

    Refactored the isochrone tests to check all the luminosities, pass pylint

--- tests/test_isochrone.py
+++ tests/test_isochrone.py
@@ -1,11 +1,16 @@
+"""
+test_isochrone
+==========
+
+This file contains unit tests for isochrone interpolation.
+"""
 import astropy.units as u
 import numpy as np
-import pytest
-from numpy.testing import assert_array_equal, assert_array_less
+from numpy.testing import assert_array_less
 from scipy.integrate import trapezoid
 
 import arsenal_gear
-from tqdm import tqdm
+
 
 def get_contiguous_regions(mask):
     """
@@ -31,86 +36,66 @@ def integrate_mask(y, x, mask):
     regions = get_contiguous_regions(mask)
     total_integral = 0.0
     for (s,e) in regions:
-        if (e-s>1):
+        if e-s>1:
             total_integral += trapezoid(y[s:e], x[s:e])
     return total_integral
 
 def test_mist_interp():
-    # initialize stellar population with EEP interpolation
-    # and otherwise default parameters (which will specify MIST isochrones)
-    # test=Ture in isochrone interpolation to elave out nearest isochrone
-    sp_eep = arsenal_gear.StellarPopulation(interp_op="eep")
-    sp_iso = arsenal_gear.StellarPopulation(interp_op="iso",test=True)
+    """ initialize stellar population with EEP interpolation
+    and otherwise default parameters (which will specify MIST isochrones)
+    test=True in isochrone interpolation to leave out nearest isochrone
+    """
+    sp = {'eep':arsenal_gear.StellarPopulation(interp_op="eep"),
+        'iso':arsenal_gear.StellarPopulation(interp_op="iso",test=True)}
 
+    lmissed = {'eep': [], 'iso': []}
+    L_err = {'eep': [], 'iso': []}
+    T_err = {'eep': [], 'iso': []}
+    T, L, nm, lum, lw_lerr = {}, {}, {}, {}, {}
 
-    (lmissed_eep, lmissed_iso) = ([],[])
-    (rel_err_eep, rel_err_iso) = ([],[])
-    (L_err_eep, L_err_iso) = ([],[])
-    (T_err_eep, T_err_iso) = ([],[])
-
-    ais = np.arange(len(sp_iso.iso.iset.lages))
+    ais = np.arange(len(sp['iso'].iso.iset.lages))
     for ai in ais[2::5]:
         ai += 1
-        t = (1+1e-6)*np.power(10,np.array([sp_iso.iso.iset.lages[ai]])-6)*u.Myr
+        t = (1+1e-6)*np.power(10,np.array([sp['iso'].iso.iset.lages[ai]])-6)*u.Myr
 
-        ms = sp_iso.iso.iset.isos[ai].qs["initial_mass"] * u.Msun
-        xi = sp_iso.imf.pdf(ms)
+        ms = sp['iso'].iso.iset.isos[ai].qs["initial_mass"] * u.Msun
+        xi = sp['iso'].imf.pdf(ms)
 
-        T_eep = (sp_eep.iso.teff(ms, t)/u.K).value
-        L_eep = (sp_eep.iso.lbol(ms, t)/u.Lsun).value
+        L_ref = np.power(10, sp['iso'].iso.iset.isos[ai].qs["log_L"])
+        T_ref = np.power(10, sp['iso'].iso.iset.isos[ai].qs["log_Teff"])
+        lum_ref = trapezoid(L_ref*xi, ms.value)
+        lw_teff_ref = trapezoid(L_ref*xi*T_ref, ms.value)/lum_ref
 
-        T_iso = (sp_iso.iso.teff(ms, t)/u.K).value
-        L_iso = (sp_iso.iso.lbol(ms, t)/u.Lsun).value
+        for k in ['eep', 'iso']:
+            T[k] = (sp[k].iso.teff(ms, t)/u.K).value
+            L[k] = (sp[k].iso.lbol(ms, t)/u.Lsun).value
 
-        L_ref = np.power(10, sp_iso.iso.iset.isos[ai].qs["log_L"])
-        T_ref = np.power(10, sp_iso.iso.iset.isos[ai].qs["log_Teff"])
-        lum = trapezoid(L_ref*xi, ms.value)
-        lw_teff = trapezoid(L_ref*xi*T_ref, ms.value)/lum
+            # fraction of the total luminosity that
+            # is missed by interpolation edge effects
+            lmissed[k].append(integrate_mask(L_ref*xi, ms.value, L[k].mask)/lum_ref)
 
-        # fraction of the total luminosity that
-        # is missed by interpolation edge effects
-        Lmiss_eep = integrate_mask(L_ref*xi, ms.value, L_eep.mask)/lum
-        Lmiss_iso = integrate_mask(L_ref*xi, ms.value, L_iso.mask)/lum
-        lmissed_eep.append(Lmiss_eep)
-        lmissed_iso.append(Lmiss_iso)
+            nm[k] = np.logical_not(L[k].mask)
 
-        neepm = np.logical_not(L_eep.mask)
-        nisom = np.logical_not(L_iso.mask)
+            lum[k] = integrate_mask(L[k]*xi, ms.value, nm[k])
+            L_err[k].append(abs(lum[k]-lum_ref)/lum_ref)
 
-        lum_eep = integrate_mask(L_eep*xi, ms.value, neepm)
-        lum_iso = integrate_mask(L_iso*xi, ms.value, nisom)
-        L_err_eep.append(abs(lum_eep-lum)/lum)
-        L_err_iso.append(abs(lum_iso-lum)/lum)
 
-        lw_teff_eep = integrate_mask(L_eep*xi*T_eep, ms.value, neepm)/lum_eep
-        lw_teff_iso = integrate_mask(L_iso*xi*T_iso, ms.value, nisom)/lum_iso
-        T_err_eep.append(abs(lw_teff_eep-lw_teff)/lw_teff)
-        T_err_iso.append(abs(lw_teff_iso-lw_teff)/lw_teff)
+            T_err[k].append(abs(integrate_mask(L[k]*xi*T[k], ms.value, nm[k])/lum[k]
+                - lw_teff_ref)/lw_teff_ref)
 
-        lw_lerr_eep = integrate_mask(np.abs(L_eep-L_ref)*xi, ms.value, neepm)/lum_eep
-        lw_lerr_iso = integrate_mask(np.abs(L_iso-L_ref)*xi, ms.value, nisom)/lum_iso
-        rel_err_eep.append(lw_lerr_eep)
-        rel_err_iso.append(lw_lerr_iso)
+            lw_lerr[k] = integrate_mask(np.abs(L[k]-L_ref)*xi, ms.value, nm[k])/lum[k]
 
-    (lmissed_eep, lmissed_iso) = (np.array(lmissed_eep), np.array(lmissed_iso))
-    (rel_err_eep, rel_err_iso) = (np.array(rel_err_eep), np.array(rel_err_iso))
-    (L_err_eep, L_err_iso) = (np.array(L_err_eep), np.array(L_err_iso))
-    (T_err_eep, T_err_iso) = (np.array(T_err_eep), np.array(T_err_iso))
-
-    # these don't seem like very stringent constraints, but there are regions
-    # where things don't seem to work perfectly well in summary metrics, even
-    # though the interpolation looks fine by eye
-    assert_array_less(lmissed_eep, 0.35)
-    assert_array_less(lmissed_iso, 0.35)
-    assert_array_less(L_err_eep, 0.50)
-    assert_array_less(T_err_eep, 0.30)
-    assert_array_less(T_err_iso, 0.30)
-
-    # the averages errors over all time, especially if you weight by luminosity
-    # over time, which we don't do, are quite small 3% or less
-    assert np.average(lmissed_eep) < 0.03
-    assert np.average(lmissed_iso) < 0.03
-    assert np.average(L_err_eep)  < 0.03
-    assert np.average(L_err_eep)  < 0.04
-    assert np.average(T_err_eep)  < 0.03
-    assert np.average(T_err_iso)  < 0.03
+    for k in ['eep', 'iso']:
+        lmissed[k] = np.array(lmissed[k])
+        L_err[k] = np.array(L_err[k])
+        T_err[k] = np.array(T_err[k])
+        # these don't seem like very stringent constraints, but there are regions
+        # where things don't seem to work perfectly well in summary metrics, even
+        # though the interpolation looks fine by eye
+        assert_array_less(lmissed[k], 0.35)
+        assert_array_less(L_err[k], 0.50)
+        assert_array_less(T_err[k], 0.30)
+        # the averages errors over all time, especially if you weight by luminosity
+        # over time, which we don't do, are quite small 3% or less
+        for arr in lmissed[k], L_err[k], T_err[k]:
+            assert np.average(arr) < 0.03
