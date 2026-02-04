5c1bc5e4432819a4750244e09234ff8cbd39a3d0
Lachlan Lancaster <lachlanlancaster@gmail.com>
2026-02-04 10:44:06 -0500

    enabled interpolation test

--- tests/test_isochrone.py
+++ tests/test_isochrone.py
@@ -45,9 +45,6 @@ def integrate_mask(y, x, mask):
     return total_integral
 
 
-@pytest.mark.skip(
-    reason="The luminosity error in the isochrone without EEP is too large in one element"
-)
 def test_mist_interp():
     """initialize stellar population with EEP interpolation
     and otherwise default parameters (which will specify MIST isochrones)
