81d1cb74343a9310cc0f5b8e558de8845760d884
Ben Keller <malzraa@gmail.com>
2026-02-05 17:37:31 -0600

    Split out the lifetime and explodability functions in the param file.

--- input/param_files/gasoline_changa.py
+++ input/param_files/gasoline_changa.py
@@ -3,22 +3,16 @@
 imf = dist_funcs.imf.Salpeter(0.08 * u.Msun, 100 * u.Msun, alpha=2.3)
 form_context = FormationContext(imf=imf, mass=1e6 * u.Msun, metals=0.01)
 
-# Evolution context: the evolve_context variable accumulates
-# everything needed to evolve a stellar population and get yields
-explodability = lambda stars, t0, t1: np.logical_and(
-    feedbacks.sn.explodable_mass_range(8 * u.Msun, 40 * u.Msun)(stars),
-    feedbacks.sn.explodable_lifetime_range(t0, t1, feedbacks.sn.lifetimes_Raiteri)(
-        stars
-    ),
-)
 
 sn = feedbacks.SNFeedbackMechanism(
-    energy_func=lambda stars: feedbacks.sn.constant_energy(stars, 1e51 * u.erg),
-    mass_func=feedbacks.sn.massloss_Raiteri,
-    metals_func=feedbacks.sn.metals_Raiteri,
-    explodability_func=explodability,
+    energy_func=lambda stars: feedbacks.analytic.constant_energy(stars, 1e51 * u.erg),
+    mass_func=feedbacks.analytic.mass_Raiteri,
+    metals_func=feedbacks.analytic.metals_Raiteri,
+    lifetime_func=feedbacks.analytic.lifetime_Raiteri,
+    explodability_func=lambda stars: feedbacks.analytic.explodable_mass_range(
+        8 * u.Msun, 40 * u.Msun
+    ),
 )
-
 evolve_context = EvolutionContext(mechanisms=[sn])
 
 # out_times is a list of times at which to output results
