8ad67e5c938a04c3027403c58289d5f317093d1e
Lachlan Lancaster <lachlanlancaster@gmail.com>
2026-02-12 11:26:08 -0600

    basics of Evolution interface

--- arsenal_gear/stellar_evolution/__init__.py
+++ arsenal_gear/stellar_evolution/__init__.py
@@ -7,4 +7,19 @@ Submodule to handle stellar evolution codes and their isochrones
 
 from . import isochrone
 
-__all__ = ["isochrone"]
+__all__ = ["Evolution"]
+
+class Evolution():
+    def __init__(self, **kwargs):
+
+        # initialize the single star evolution interface
+        self.se = kwargs.get("single_evolution", "interpolator")
+        if not isinstance(self.se, isochrone.AbstractIsochrone):
+            if self.se == "interpolator":
+                self.se = isochrone.IsochroneInterpolator(**kwargs)
+            else:
+                err_msg = f"Invalid single_evolution option: {self.se}"
+                raise ValueError(err_msg)
+        
+        # initialize the binary star evolution interface
+        self.be = None
